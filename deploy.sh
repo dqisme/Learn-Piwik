c24c603964ebf2f218a488e5392e6bfa44ee8b9e
dqisme <dqisme@outlook.com>
2016-08-10 20:57:54 +0800

    [dq] #N/A add piwik link alias and fix it in nginx config

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ docker run --name $db_service_name -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$db_passw
 docker run --name $piwik_service_name --link $db_service_name:db -p 9000:9000 -v `pwd`/config:/var/www/html/config -d piwik
 
 sed "s/\$piwik_service_name/$piwik_service_name/g" nginx.conf.template > ./nginx.conf
-docker run --name $http_service_name -p 80:80 --link $piwik_service_name --volumes-from $piwik_service_name -d nginx
+docker run --name $http_service_name -p 80:80 --link $piwik_service_name:app --volumes-from $piwik_service_name -d nginx
 docker cp ./nginx.conf $http_service_name:/etc/nginx/nginx.conf
 docker restart $http_service_name
 rm ./nginx.conf
