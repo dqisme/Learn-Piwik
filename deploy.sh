c8afad3b8373d8d5ea88ebf47b0dbdac50226a1d
dqisme <dqisme@outlook.com>
2016-08-10 21:00:32 +0800

    [dq] #N/A mount nginx config instead of copying it

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,4 @@ docker run --name $db_service_name -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$db_passw
 
 docker run --name $piwik_service_name --link $db_service_name:db -p 9000:9000 -v `pwd`/config:/var/www/html/config -d piwik
 
-docker run --name $http_service_name -p 80:80 --link $piwik_service_name:app --volumes-from $piwik_service_name -d nginx
-docker cp ./nginx.conf $http_service_name:/etc/nginx/nginx.conf
-docker restart $http_service_name
+docker run --name $http_service_name -p 80:80 --link $piwik_service_name:app -v `pwd`/nginx.conf:/etc/nginx/nginx.conf:ro --volumes-from $piwik_service_name -d nginx
