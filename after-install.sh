0d403197a45c29d969f0800985c32ac57464c3d9
dqisme <dqisme@outlook.com>
2016-08-10 17:13:46 +0800

    [dq] modify mount config directly after instalation

--- after-install.sh
+++ after-install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 domain_name=localhost:8080
-service_name=lsc-piwik
-http_service_name=$service_name-http
-
-docker exec $http_service_name sed -i "/trusted_hosts/s/localhost/$domain_name/" /var/www/html/config/config.ini.php
+sed "/trusted_hosts/s/localhost/$domain_name/" ./config/config.ini.php > ./config/config.ini.php
