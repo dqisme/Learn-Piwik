d1dfbef53af0c8187ee6a53b9e544fc3f4be2c5f
dqisme <dqisme@outlook.com>
2016-08-10 17:42:59 +0800

    [dq] #N/A fix modifying config

--- after-install.sh
+++ after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 domain_name=localhost:8080
-sed "/trusted_hosts/s/localhost/$domain_name/" ./config/config.ini.php > ./config/config.ini.php
+sed "/trusted_hosts/s/localhost/$domain_name/" ./config/config.ini.php | tee ./config/config.ini.php
